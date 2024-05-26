b82ed8b55a58d68049a0fa0c338d46e4f59fcaed
senosq <149302749+senosq@users.noreply.github.com>
2024-05-26 14:27:06 +0300

    Add files via upload

--- Sedona/script.js
+++ Sedona/script.js
@@ -1,38 +1,55 @@
 document.addEventListener("DOMContentLoaded",function(){
-const buttonForModalWindow = document.getElementById("btnForModalWindow");
-const modalWindow = document.getElementById("modWindow")
-const numberInput = document.getElementById("leavingAdult");
-const minusBtn = document.getElementById("minusButton");
-const plusBtn = document.getElementById("lastBtn");
-const numberInput2 = document.getElementById("leavingChildren");
-const minusBtnCd = document.getElementById("minusButtonCd");
-const plusBtnCd = document.getElementById("plusButtonCd");
-
-plusBtn.addEventListener("click",function(){
-    let value = parseInt(numberInput.value);
-    if(value<=100){
-    numberInput.value = value+1;
-    }
-});
-
-minusBtn.addEventListener("click",function(){
-    let value = parseInt(numberInput.value);
-    if(value>1){
-    numberInput.value = value-1;
-    }
-});
-
-plusBtnCd.addEventListener("click",function(){
-    let value = parseInt(numberInput2.value);
-    if(value<=100){
-    numberInput2.value = value+1;
-    }
-});
-
-minusBtnCd.addEventListener("click",function(){
-    let value = parseInt(numberInput2.value);
-    if(value>0){
-    numberInput2.value = value-1;
-    }
-});
-});
+    const buttonForModalWindow = document.getElementById("btnForModalWindow");
+    const modalWindow = document.getElementById("modWindow")
+    
+    
+    const numberInput = document.getElementById("leavingAdult");
+    const minusBtn = document.getElementById("minusButton");
+    const plusBtn = document.getElementById("lastBtn");
+    const numberInput2 = document.getElementById("leavingChildren");
+    const minusBtnCd = document.getElementById("minusButtonCd");
+    const plusBtnCd = document.getElementById("plusButtonCd");
+    
+    buttonForModalWindow.addEventListener("click", function(){
+          if(modalWindow.classList.contains("modalDis")){
+            modalWindow.classList.remove("modalDis");
+          }
+          else{
+            modalWindow.classList.toggle("modalDis");
+          }
+    });
+    
+    window.addEventListener("keydown", function(evt){
+        if(evt.key === 'Escape' && !modalWindow.classList.contains("modalDis")){
+            modalWindow.classList.toggle("modalDis");
+        }
+    });
+    
+    plusBtn.addEventListener("click",function(){
+        let value = parseInt(numberInput.value);
+        if(value<=100){
+        numberInput.value = value+1;
+        }
+    });
+    
+    minusBtn.addEventListener("click",function(){
+        let value = parseInt(numberInput.value);
+        if(value>1){
+        numberInput.value = value-1;
+        }
+    });
+    
+    plusBtnCd.addEventListener("click",function(){
+        let value = parseInt(numberInput2.value);
+        if(value<=100){
+        numberInput2.value = value+1;
+        }
+    });
+    
+    minusBtnCd.addEventListener("click",function(){
+        let value = parseInt(numberInput2.value);
+        if(value>0){
+        numberInput2.value = value-1;
+        }
+    });
+    });
